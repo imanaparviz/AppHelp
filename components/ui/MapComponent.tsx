c67636de32561c0616557cf613247f35ae8b4fc6
imanaparviz <119262937+imanaparviz@users.noreply.github.com>
2024-11-01 11:18:58 +0100

    Update MapComponent.tsx

--- components/ui/MapComponent.tsx
+++ components/ui/MapComponent.tsx
@@ -3,6 +3,7 @@
 import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet'
 import 'leaflet/dist/leaflet.css'
 import L from 'leaflet'
+import { useEffect } from 'react'
 
 // Fix for default marker icon in react-leaflet
 delete L.Icon.Default.prototype._getIconUrl;
@@ -12,10 +13,21 @@ L.Icon.Default.mergeOptions({
   shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.3.1/images/marker-shadow.png',
 });
 
-function MapCenterUpdater({ setMapCenter }) {
-  const map = useMap();
-  // ... rest of your MapCenterUpdater code ...
-  return null;
+function MapCenterUpdater({ setMapCenter }: { setMapCenter: (center: [number, number]) => void }) {
+  const map = useMap()
+
+  useEffect(() => {
+    // Use map and setMapCenter in the effect
+    const updateMapCenter = async () => {
+      // ... existing code ...
+      if (map) {
+        map.setView(newCenter, 13)
+      }
+    }
+    updateMapCenter()
+  }, [map, setMapCenter])
+
+  return null
 }
 
 export default function MapComponent({ mapCenter, setMapCenter, nearbyProviders }) {
